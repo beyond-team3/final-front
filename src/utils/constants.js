208ee3bae83f19c7c1171708306d21e07fc671fe
hatahng <hathang16@naver.com>
2026-03-17 10:43:09 +0900

    design/ui-frt-92 : 문서에 "작성" 붙이기

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -35,11 +35,11 @@ export const MENU_CONFIG = [
         label: '문서 관리',
         roles: [ROLES.SALES_REP, ROLES.ADMIN, ROLES.CLIENT],
         children: [
-            { key: 'quote-request', label: '견적 요청서', route: '/documents/request', roles: [ROLES.CLIENT] },
-            { key: 'quote', label: '견적서', route: '/documents/quotation', roles: [ROLES.SALES_REP] },
-            { key: 'contract', label: '계약서', route: '/documents/contract', roles: [ROLES.SALES_REP] },
-            { key: 'order', label: '주문서', route: '/documents/order', roles: [ROLES.CLIENT] },
-            { key: 'statement', label: '청구서', route: '/documents/invoices', roles: [ROLES.SALES_REP, ROLES.ADMIN] },
+            { key: 'quote-request', label: '견적 요청서 작성', route: '/documents/request', roles: [ROLES.CLIENT] },
+            { key: 'quote', label: '견적서 작성', route: '/documents/quotation', roles: [ROLES.SALES_REP] },
+            { key: 'contract', label: '계약서 작성', route: '/documents/contract', roles: [ROLES.SALES_REP] },
+            { key: 'order', label: '주문서 작성', route: '/documents/order', roles: [ROLES.CLIENT] },
+            { key: 'statement', label: '청구서 작성', route: '/documents/invoices', roles: [ROLES.SALES_REP, ROLES.ADMIN] },
 
             { key: 'document-history', label: '영업 히스토리', route: '/documents/history', roles: [ROLES.SALES_REP, ROLES.ADMIN] },
             { key: 'document-all', label: '모든 문서', route: '/documents/all', roles: [ROLES.SALES_REP, ROLES.ADMIN, ROLES.CLIENT] },
