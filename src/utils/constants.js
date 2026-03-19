b677343be6b974500e742cf9218264612cbfe175
hatahng <hathang16@naver.com>
2026-03-19 20:04:40 +0900

    design/ui-frt-92 : 거래처 사이드바 영업히스토리 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -41,7 +41,7 @@ export const MENU_CONFIG = [
             { key: 'order', label: '주문서 작성', route: '/documents/order', roles: [ROLES.CLIENT] },
             { key: 'statement', label: '청구서 작성', route: '/documents/invoices', roles: [ROLES.SALES_REP, ROLES.ADMIN] },
 
-            { key: 'document-history', label: '영업 히스토리', route: '/documents/history', roles: [ROLES.SALES_REP, ROLES.ADMIN] },
+            { key: 'document-history', label: '영업 히스토리', route: '/documents/history', roles: [ROLES.SALES_REP, ROLES.ADMIN, ROLES.CLIENT] },
             { key: 'document-all', label: '모든 문서', route: '/documents/all', roles: [ROLES.SALES_REP, ROLES.ADMIN, ROLES.CLIENT] },
             { key: 'approval', label: '승인 관리', route: '/approval', roles: [ROLES.SALES_REP, ROLES.ADMIN, ROLES.CLIENT] },
         ],
